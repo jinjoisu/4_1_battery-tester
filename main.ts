c95e9948c74da0c101e5102849e55f58fcb21ff1
jinjoisu <133915914+jinjoisu@users.noreply.github.com>
2023-05-26 14:04:23 -0500

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -2,6 +2,16 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(pins.analogReadPin(AnalogPin.P0))
     basic.pause(2000)
 })
+input.onButtonPressed(Button.AB, function () {
+    reading = pins.analogReadPin(AnalogPin.P0)
+    voltage = reading * 1000 / 340
+    if (voltage >= 1000) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (voltage < 1000) {
+        basic.showIcon(IconNames.No)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     reading = pins.analogReadPin(AnalogPin.P0)
     voltage = reading * 1000 / 340
